aa8745f2726d058f6c7b5e402cea2a4ac189338e
David Oti <davmixcool@gmail.com>
2021-04-16 23:42:29 +0100

    Add config to .env file

--- common/config.js
+++
@@ -1,5 +0,0 @@
-const LocalRpcProvider = 'http://127.0.0.1:7545';
-const InfuraRpcProvider = '';
-exports.RpcProvider = LocalRpcProvider;
-exports.EtherScanKey = 'M3WPU63N74CM5SHPGS2UCICZUSF79BNV96';
-exports.ApiKey = '2UDB3dHF79h96yVVHvY6c6d51SEU501XwBj';
